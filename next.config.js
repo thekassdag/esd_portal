32f131e5f6c053409e07f3cfb62e027fc5416374
dagi afro <dagimtheteencoder@gmail.com>
2026-06-09 11:01:52 +0300

    refactor: migrate database client to Prisma and update Next.js environment configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['localhost:3001'],
-  // allowedDevOrigins: ['edc.antsar.et'],
+  // allowedDevOrigins: ['localhost:3001'],
+  allowedDevOrigins: ['edc.antsar.et'],
 };
 
 export default nextConfig;
